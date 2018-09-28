8ff8d668f8a1a62d27d84905b43acfc71435389d
supanut1911 <ynotmela@gmail.com>
2018-09-28 23:15:35 +0700

    Router

--- App.js
+++ App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather';
 
+import { createStackNavigator } from 'react-navigation';
+import WeatherScreen from './components/WeatherScreen';
+import ZipCodeScreen from './components/ZipCodeScreen';
+
+const RootStack = createStackNavigator({
+  Weather: WeatherScreen,
+  ZipCode: ZipCodeScreen
+}, {
+    initialRouteName: 'Weather',
+  })
+
 export default class App extends React.Component {
   render() {
     return (
-
-      <Weather zipCode="90110" />
-
+      <RootStack />
     );
   }
 }
