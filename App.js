c1af3da3454ac8031e52b1c0049472c8a849f32c
supanut1911 <ynotmela@gmail.com>
2018-09-28 20:59:35 +0700

    add component

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import Weather from './components/Weather'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text onPress = {this.doit}>Hello World!</Text>
-      </View>
+      <Weather zipCode="90110"/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
